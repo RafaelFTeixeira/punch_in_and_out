017e201c4f5d0fcd2321dcafaaf8ca3a89f96145
Rafael Teixeira <p51801407@brasilprev.com.br>
2019-11-18 18:54:56 -0300

    create scene with three punch clock

--- domain/user.py
+++ domain/user.py
@@ -12,14 +12,23 @@ class User:
         self.punches.append(Punch())
 
     def get_work_hours_today(self):
-        today = datetime.now()
-        punchesToday = filter(
-            lambda punch: punch.day == today.day, self.punches)
-        punchesToday = list(punchesToday)
+        punchesToday = self.get_punches_today()
+        count_punches_today = self.__count_punches(punchesToday)
 
         hours = 0
-        for index in range(0, len(punchesToday), 2):
+        for index in range(0, count_punches_today, 2):
             start = punchesToday[index]
             end = punchesToday[index + 1]
             hours = end.hour - start.hour
         return hours
+
+    def get_punches_today(self):
+        today = datetime.now()
+        punchesToday = filter(
+            lambda punch: punch.day == today.day, self.punches)
+        return list(punchesToday)
+
+    @staticmethod
+    def __count_punches(punches):
+        count_punches = len(punches)
+        return count_punches if count_punches % 2 == 0 else count_punches - 1
--- unit_test/test_user.py
+++ unit_test/test_user.py
@@ -4,6 +4,9 @@ from domain.user import User
 
 
 class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User('Rafael Teixeira', 'rafaelteixeiradev@gmail.com')
+
     def test_should_create_an_user(self):
         user_expected = {
             'name': 'Rafael Teixeira',
@@ -17,14 +20,31 @@ class UserTest(unittest.TestCase):
 
     def test_should_get_work_hours_today(self):
         hour_expected = 1
-        user = User('Rafael Teixeira', 'rafaelteixeiradev@gmail.com')
         punch_in = datetime.now()
         punch_out = datetime.now() + timedelta(hours=hour_expected)
-        user.punches = [punch_in, punch_out]
+        self.user.punches = [punch_in, punch_out]
 
-        hours = user.get_work_hours_today()
+        hours = self.user.get_work_hours_today()
 
         self.assertEqual(hour_expected, hours)
+
+    def test_should_return_zero_hour_when_no_has_two_punch_clock(self):
+        punch_in = datetime.now()
+        self.user.punches = [punch_in]
+
+        hours = self.user.get_work_hours_today()
+
+        self.assertEqual(0, hours)
+
+    def test_should_return_sum_hours_with_start_and_end(self):
+        punch_in = datetime.now()
+        punch_out = datetime.now() + timedelta(hours=1)
+        punch_in2 = datetime.now() + timedelta(hours=2)
+        self.user.punches = [punch_in, punch_out, punch_in2]
+
+        hours = self.user.get_work_hours_today()
+
+        self.assertEqual(1, hours)
 
 
 if __name__ == "__main__":
