5a5e773395e5762268a577338d74115948c07eb3
Rafael Teixeira <p51801407@brasilprev.com.br>
2019-11-20 19:47:40 -0300

    insert relation ship punches

--- domain/punch.py
+++ domain/punch.py
@@ -3,4 +3,5 @@ from datetime import datetime
 
 class Punch:
     def __init__(self):
+        self.id = 0
         self.date = datetime.now()
--- infrastructure/mapper/user.py
+++ infrastructure/mapper/user.py
@@ -4,6 +4,6 @@ from infrastructure import db
 
 class User(db.Model, UserDomain):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    # pumches = db.relationship('Punch', backred='punches')
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    punches = db.relationship('Punch')
